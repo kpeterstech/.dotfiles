8121ba155adf19ef537c408a78045546e5cf2fd0
kpeterstech <kpeterstech@gmail.com>
2018-03-04 21:08:36 -0600

    edited docstrings for ffile_renumber.py

--- bin/ffile_renumber.py
+++ bin/ffile_renumber.py
@@ -5,13 +5,15 @@ import os
 
 class Renumber():
     """A class used to find files and renumber them based off a
-    prefix that is passed to it when the class is instanciated"""
+    prefix that is passed to it when the class is instanciated
+    """
 
     def __init__(self, file_prefix, path):
         """Variables that __init__ creates:
-            count - the initial count that will be used when renaming files
-            prefix - what will be added to the front of all renumber_files
-            path - the path where the renumbering will begin to take place"""
+            :param count: An Int, the initial count that will be used when renaming files
+            :param prefix: A String, what will be added to the front of all renumber_files
+            :param path: A String, the path where the renumbering will begin to take place
+        """
 
         self.count = 1
         self.prefix = file_prefix
@@ -29,7 +31,8 @@ class Renumber():
 
     def find_dirs(self):
         """Finds all subdirectors under the root dir (the path that was passed).
-            All subdirectories that are found are placed into a list, sorted, then returned"""
+            All subdirectories that are found are placed into a list, sorted, then returned
+        """
 
         all_dirs_list = []
         for root, dirs, files in os.walk(self.path):
@@ -40,9 +43,10 @@ class Renumber():
 
     def renumber_files(self, dir_list):
         """Takes in a list with directory paths. It then iterates over the directories
-        and looks for all files in each one. If a file is found it is added to a files list.
-        Once all files are found in the current directory, the files are renumbered and it then
-        proceedes to the next directory"""
+            and looks for all files in each one. If a file is found it is added to a files list.
+            Once all files are found in the current directory, the files are renumbered and it then
+            proceedes to the next directory
+        """
 
         for dir in dir_list:
             os.chdir(dir)
