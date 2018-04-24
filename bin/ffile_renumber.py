41ffe2b474d9f1244853491b9a5d51bc6952259d
kpeterstech <kpeterstech@gmail.com>
2018-04-24 17:42:51 -0500

    ffile_renumber.py had a bug where in the process of renumbering files it would end up accidently remove the by overwriting the same file, just with a different name.
    The solution implimented was to create a new for loop within the
    "renumber_files" method. The new for loop first renames the files
    to a random name before the final for loop renames the files to their
    correct and final name

--- bin/ffile_renumber.py
+++ bin/ffile_renumber.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3.6
 
 import os
+import re
+
+# used for sorting files in naturl order. See function "natural_sort" in class Renumber
+_nsre = re.compile('([0-9]+)')
 
 
 class Renumber():
-    """A class used to find files and renumber them based off a
-    prefix that is passed to it when the class is instanciated
+    """
+    A class used to find files and renumber them based off a
+    prefix that is passed to it when the class is instansiated
     """
 
     def __init__(self, file_prefix, path):
-        """Variables that __init__ creates:
+        """
+            Variables that __init__ creates:
             :param count: An Int, the initial count that will be used when renaming files
             :param prefix: A String, what will be added to the front of all renumber_files
             :param path: A String, the path where the renumbering will begin to take place
         """
 
-        self.count = 1
+        self.count = 0
+        self.tmp_count = 0
         self.prefix = file_prefix
         self.path = path
 
@@ -29,8 +36,14 @@ class Renumber():
 
         print(self.path)
 
+    def natural_sort(self, s):
+        """ takes in a list and sorts it in a natural sort order """
+        return [int(text) if text.isdigit() else text.lower()
+                for text in re.split(_nsre, s)]
+
     def find_dirs(self):
-        """Finds all subdirectors under the root dir (the path that was passed).
+        """
+            Finds all subdirectors under the root dir (the path that was passed).
             All subdirectories that are found are placed into a list, sorted, then returned
         """
 
@@ -42,7 +55,8 @@ class Renumber():
         return all_dirs_list
 
     def renumber_files(self, dir_list):
-        """Takes in a list with directory paths. It then iterates over the directories
+        """
+            Takes in a list with directory paths. It then iterates over the directories
             and looks for all files in each one. If a file is found it is added to a files list.
             Once all files are found in the current directory, the files are renumbered and it then
             proceedes to the next directory
@@ -54,11 +68,23 @@ class Renumber():
             for entry in os.listdir('.'):
                 if os.path.isfile(entry):
                     files.append(entry)
-            files.sort()
-            for file in files:
-                filename, extension = os.path.splitext(file)
-                os.rename(file, f'{self.prefix} - {self.count}{extension}')
+
+            files.sort(key=self.natural_sort)
+
+            tmp_files = []
+
+            for tmp_file in files:
+                self.tmp_count += 1
+                filename, extension = os.path.splitext(tmp_file)
+                tmp_name = f'G5G74W - {self.tmp_count}{extension}'
+                tmp_files.append(tmp_name)
+                os.rename(tmp_file, tmp_name)
+
+            for file in tmp_files:
                 self.count += 1
+                filename, extension = os.path.splitext(file)
+                new_filename = f'{self.prefix} - {self.count}{extension}'
+                os.rename(file, new_filename)
 
 
 def main():
@@ -69,10 +95,10 @@ def main():
     dir_name = os.path.split(path)
     file_prefix = dir_name[-1]
 
-    # instantiate the class that will handle all the renumbering and pass the prefix
+    # instantiate the class that will handle all the renumbering and pass the prefix and path
     renumber = Renumber(file_prefix, path)
 
-    # find all dirs/subdirs in the path.
+    # find all subdirs in the path.
     dirs = renumber.find_dirs()
 
     # renumber all the files that are found
