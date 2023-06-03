fbc8f38c89238541389ece821b69f635d216a244
kpeterstech <kpeterstech@gmail.com>
2023-06-03 15:14:31 -0700

    changed extract fish function

--- fish/functions/extract.fish
+++ fish/functions/extract.fish
@@ -1,38 +1,41 @@
 function extract
     for file in $argv
         switch $file
-            case *.tar.bz2
+            case "*.tar.bz2"
                 tar xjf $file
 
-            case *.tar.gz
+            case "*.tar.gz"
                 tar xzf $file
 
-            case *.bz2
+            case "*.bz2"
                 bunzip2 $file
 
-            case *.rar
+            case "*.rar"
                 unrar e $file
 
-            case *.gz
+            case "*.gz"
                 gunzip $file
 
-            case *.tar
+            case "*.tar"
                 tar xf $file
 
-            case *.tbz2
+            case "*.tbz2"
                 tar xjf $file
 
-            case *.tgz
+            case "*.tgz"
                 tar xzf $file
 
-            case *.zip
+            case "*.zip"
                 unzip $file
 
-            case *.Z
+            case "*.Z"
                 uncompress $file
 
-            case *.7z
+            case "*.7z"
                 7z x $file
+
+            case "*"
+                echo "unknown extension: $file"
         end
     end
 end
